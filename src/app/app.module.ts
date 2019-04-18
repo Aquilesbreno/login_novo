db3d899a2eec56c6bf6f828e89d6177a1aab672e
AQUILES BRENO GOMES MEIRELES <aquiles.bgmeireles@senacsp.edu.br>
2019-04-17 22:05:22 -0300

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HomePage } from '../pages/home/home';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AutenticaProvider } from '../providers/autentica/autentica';
+import { ServicosProvider } from '../providers/servicos/servicos';
+import { CategoriasservicosProvider } from '../providers/categoriasservicos/categoriasservicos';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import { AutenticaProvider } from '../providers/autentica/autentica';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AutenticaProvider
+    AutenticaProvider,
+    ServicosProvider,
+    CategoriasservicosProvider
   ]
 })
 export class AppModule {}
